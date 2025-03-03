f62e2709da68121a13f2c85c38015d615c28fb4b
Sashlin Dean Moonsamy <sashlindean.moonsamy@gmail.com>
2025-03-03 14:14:58 +0200

    congiguired documentation for api

--- API/urls.py
+++ API/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, include
 from .views import CategoryViewSet, ProductViewSet, OrderViewSet, OrderItemViewSet, ReviewViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+
+# Documentation setup
+schema_view = get_schema_view(
+   openapi.Info(
+      title="E-Commerce API",
+      default_version='v1',
+      description="E-commerce platform API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@ecommerce.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
 )
 
 # Create a router and register our viewsets with it.
@@ -16,8 +28,9 @@ router.register(r'reviews', ReviewViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.as_view(), name='swagger-docs'),  # Swagger Docs
     path('', include(router.urls)),
 
 ]
