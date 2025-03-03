999cd2ff18529682e3ebfcfd879abdc0e6829ae2
Sashlin Dean Moonsamy <sashlindean.moonsamy@gmail.com>
2025-03-03 13:54:45 +0200

    set up the JWT authentication endpoints and the viewsets

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet, OrderViewSet, OrderItemViewSet, ReviewViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -12,5 +16,8 @@ router.register(r'reviews', ReviewViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
+
 ]
